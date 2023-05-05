08eb246a3b23a275cc56bc16933ff484f6150700
Corey Bright <95330156+cbrig5@users.noreply.github.com>
2023-05-04 22:11:32 -0400

    Update constraints.sql

--- constraints.sql
+++ constraints.sql
@@ -1,10 +1,10 @@
 ALTER TABLE appointment
-  ADD CONSTRAINT location_domain CHECK (location IN ("online", "in-person", "Online", "In-person"));
+  	ADD CONSTRAINT location_domain CHECK (location IN ("online", "in-person", "Online", "In-person"));
 
 ALTER TABLE doctor_availability 
 	ADD CONSTRAINT start_time_domain CHECK (start_time BETWEEN "08:00:00" AND "18:00:00"),
-    ADD CONSTRAINT end_time_domain CHECK(end_time BETWEEN "08:00:00" AND "18:00:00"),
-    ADD CONSTRAINT time_overlap CHECK (end_time > start_time);
+    	ADD CONSTRAINT end_time_domain CHECK(end_time BETWEEN "08:00:00" AND "18:00:00"),
+    	ADD CONSTRAINT time_overlap CHECK (end_time > start_time);
 
 ALTER TABLE insurance
 	ADD CONSTRAINT policy_group_chk CHECK (policy_number != group_number);
@@ -17,6 +17,7 @@ ALTER TABLE person
 	
 ALTER TABLE employee
 	ADD CONSTRAINT employee_end_date_chk CHECK (start_date < end_date);
+	ADD CONSTRAINT employee_email_chk CHECK (employee_email LIKE '%@%.%');
 
 ALTER TABLE telephone
 	ADD CONSTRAINT len_number_chk CHECK (LENGTH(telephone) = 10);
